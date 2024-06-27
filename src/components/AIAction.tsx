8ca6aad5fc01c727d32cc9b4153fc6e57c7a75c1
Pierre Mouchan <pierre.mouchan@gmail.com>
2024-06-27 09:12:43 +0200

    chore(update): update Anthropic to Sonnet 3.5

--- src/components/AIAction.tsx
+++ src/components/AIAction.tsx
@@ -1,9 +1,10 @@
 import { Action, Clipboard, Keyboard, getSelectedText, popToRoot, showHUD } from "@raycast/api";
-import { exec  } from "child_process";
+import { exec } from "child_process";
 import PREFERENCES from "../utils/preferences";
 import Anthropic from "@anthropic-ai/sdk";
 import OpenAI from "openai";
 import Groq from "groq-sdk";
+import { TextBlock } from "@anthropic-ai/sdk/resources";
 
 const openai = new OpenAI({
   apiKey: PREFERENCES.apiKey,
@@ -15,47 +16,35 @@ const groq = new Groq({
   apiKey: PREFERENCES.apiKey,
 });
 
-
-
 type PropsType = {
   instruction: {
-    system: string
+    system: string;
     userMessageWrapper: {
-      start: string
-      end: string
-    }
-    submitText?: string
-  }
-  index: number
-}
+      start: string;
+      end: string;
+    };
+    submitText?: string;
+  };
+  index: number;
+};
 
 const AIAction = ({ instruction, index }: PropsType) => {
-
   const handleSendText = async () => {
     try {
       // play sound and show loading state
       await showHUD("Loading...");
-  exec('afplay /System/Library/Sounds/Frog.aiff');
+      exec("afplay /System/Library/Sounds/Frog.aiff");
 
-      let response: string | null= null
-
-
+      let response: string | null = null;
 
       // get selected text from frontmost app
       const selectedText = await getSelectedText();
-      console.log(instruction.system)
-     console.log(`
-             ${instruction.userMessageWrapper.start}
-             ${selectedText}
-             ${instruction.userMessageWrapper.end}
-
-     `)
 
       // if the API key starts with "sk-", use the anthropic API
       if (PREFERENCES.apiKey.startsWith("sk-ant-api")) {
         // send request and action to anthropic
         const res = await anthropic.messages.create({
-          model: "claude-3-haiku-20240307",
+          model: "claude-3-5-sonnet-20240620",
           max_tokens: 1024,
           system: instruction.system,
           messages: [
@@ -65,18 +54,19 @@ const AIAction = ({ instruction, index }: PropsType) => {
              ${instruction.userMessageWrapper.start}
              ${selectedText}
              ${instruction.userMessageWrapper.end}
-            `
+            `,
             },
           ],
         });
+
         // get back the response from anthropic
-       response = res.content[0].text
+        response = (res.content[0] as TextBlock).text
 
         // else if the API key starts with "gsk_", use the groq API
       } else if (PREFERENCES.apiKey.startsWith("gsk_")) {
         // send the request to groq
         const chatCompletion = await groq.chat.completions.create({
-          model: 'llama3-70b-8192',
+          model: "llama3-70b-8192",
           messages: [
             {
               role: "system",
@@ -88,57 +78,48 @@ const AIAction = ({ instruction, index }: PropsType) => {
              ${instruction.userMessageWrapper.start}
              ${selectedText}
              ${instruction.userMessageWrapper.end}
-            `
+            `,
             },
           ],
-            })
+        });
 
-        response = chatCompletion.choices[0].message.content
-      } else{
-
-
-      // send request and action to OpenAI
-      const res = await openai.chat.completions.create({
-        model: "gpt-4o",
-        max_tokens: 1024,
-        messages: [
-          {
-            role: "system",
-            content: instruction.system,
-          },
-          {
-            role: "user",
-            content: `
+        response = chatCompletion.choices[0].message.content;
+      } else {
+        // send request and action to OpenAI
+        const res = await openai.chat.completions.create({
+          model: "gpt-4o",
+          max_tokens: 1024,
+          messages: [
+            {
+              role: "system",
+              content: instruction.system,
+            },
+            {
+              role: "user",
+              content: `
              ${instruction.userMessageWrapper.start}
              ${selectedText}
              ${instruction.userMessageWrapper.end}
-            `
-          },
-        ],
-      });
+            `,
+            },
+          ],
+        });
 
-      // get back the response from OpenAI
-       response = res.choices[0].message.content;
-
+        // get back the response from OpenAI
+        response = res.choices[0].message.content;
       }
-
-
-
-
 
       // check if response is empty
       if (!response) {
         throw new Error("Failed to get response from AI");
       }
 
-
       // copy response to clipboard and show success message + sound
       await Clipboard.copy(response);
       await showHUD("Copied back response to clipboard");
-  exec('afplay /System/Library/Sounds/Blow.aiff');
-
+      exec("afplay /System/Library/Sounds/Blow.aiff");
     } catch (error) {
-      console.log(error)
+      console.log(error);
       showHUD("Failed to get selected text or get response from ChatGPT. Please try again.");
     } finally {
       // close the app
@@ -146,10 +127,13 @@ const AIAction = ({ instruction, index }: PropsType) => {
     }
   };
 
-
-  return <Action.SubmitForm
-  shortcut={{ modifiers: ["cmd"], key: index.toString() as Keyboard.KeyEquivalent }}
-    title={instruction.submitText || "Select"} onSubmit={handleSendText} />
+  return (
+    <Action.SubmitForm
+      shortcut={{ modifiers: ["cmd"], key: index.toString() as Keyboard.KeyEquivalent }}
+      title={instruction.submitText || "Select"}
+      onSubmit={handleSendText}
+    />
+  );
 };
 
 export default AIAction;
--- src/index.tsx
+++ src/index.tsx
@@ -3,16 +3,13 @@ import AIAction from "./components/AIAction";
 import { useEffect, useState } from "react";
 import { DEFAULT_ACTIONS, DEFAULT_ACTIONS_PARAMS, DefaultActionsType } from "./utils/actions";
 
-
 import PREFERENCES from "./utils/preferences";
 import parseCSV from "./utils/parse-csv";
-
 
 export default function Command() {
   const [searchText, setSearchText] = useState("");
   const [availableActions, setAvailableActions] = useState<Record<string, DefaultActionsType> | null>(null);
   const [filteredActions, setFilteredActions] = useState<string[] | null>(null);
-
 
   useEffect(() => {
     async function getActions() {
@@ -21,36 +18,34 @@ export default function Command() {
         setAvailableActions(DEFAULT_ACTIONS);
         return;
       }
-       // else parse the csv file and set the available actions
-      const parsedData = await parseCSV(PREFERENCES.commands)
+      // else parse the csv file and set the available actions
+      const parsedData = await parseCSV(PREFERENCES.commands);
       const actions: Record<string, DefaultActionsType> = {};
       parsedData.forEach((item: DefaultActionsType) => {
         actions[item.name] = item;
       });
       setAvailableActions(actions);
     }
-    getActions()
-  },[])
+    getActions();
+  }, []);
 
-    // if there are available actions, filter them based on the search text
+  // if there are available actions, filter them based on the search text
   useEffect(() => {
     if (availableActions) {
-    if (searchText.length > 0) {
-        setFilteredActions(Object.keys(availableActions).filter((item) => item.toLowerCase().includes(searchText.toLowerCase())));
-    } else {
-      setFilteredActions(Object.keys(availableActions));
-    }
+      if (searchText.length > 0) {
+        setFilteredActions(
+          Object.keys(availableActions).filter((item) => item.toLowerCase().includes(searchText.toLowerCase())),
+        );
+      } else {
+        setFilteredActions(Object.keys(availableActions));
+      }
     }
   }, [searchText, availableActions]);
 
   return (
-    <List
-    isLoading={!filteredActions}
-      onSearchTextChange={setSearchText}
-    >
-      {availableActions && filteredActions && filteredActions.length > 0?
-
-          <>
+    <List isLoading={!filteredActions} onSearchTextChange={setSearchText}>
+      {availableActions && filteredActions && filteredActions.length > 0 ? (
+        <>
           {filteredActions.map((action, index) => (
             <List.Item
               accessories={[{ text: `${index + 1}` }]}
@@ -66,44 +61,40 @@ export default function Command() {
                       userMessageWrapper: {
                         start: DEFAULT_ACTIONS_PARAMS.USER_MESSAGE_WRAPPER_START,
                         end: DEFAULT_ACTIONS_PARAMS.USER_MESSAGE_WRAPPER_END,
-                      }
-                    }} />
+                      },
+                    }}
+                  />
                 </ActionPanel>
               }
             />
-          ))
-          }
-    </>
-        :
-
+          ))}
+        </>
+      ) : (
         // if there are no available actions, use the user's search text as the action
-        (
-          <List.Item
-            accessories={[{ text: `1` }]}
-            icon={'🙋'}
-            title={searchText}
-            actions={
-              <ActionPanel>
-                <AIAction
-                  index={1}
-                  instruction={{
-                    system: `
+        <List.Item
+          accessories={[{ text: `1` }]}
+          icon={"🙋"}
+          title={searchText}
+          actions={
+            <ActionPanel>
+              <AIAction
+                index={1}
+                instruction={{
+                  system: `
 ${searchText}
 - do not output the ${DEFAULT_ACTIONS_PARAMS.USER_MESSAGE_WRAPPER_START} and ${DEFAULT_ACTIONS_PARAMS.USER_MESSAGE_WRAPPER_END} tags.
                     `,
-                    userMessageWrapper: {
-                      start: DEFAULT_ACTIONS_PARAMS.USER_MESSAGE_WRAPPER_START,
-                      end: DEFAULT_ACTIONS_PARAMS.USER_MESSAGE_WRAPPER_END,
-                    },
-                    submitText: "Ask"
-                  }} />
-              </ActionPanel>
-            }
-          />
-
-        )
-      }
-
+                  userMessageWrapper: {
+                    start: DEFAULT_ACTIONS_PARAMS.USER_MESSAGE_WRAPPER_START,
+                    end: DEFAULT_ACTIONS_PARAMS.USER_MESSAGE_WRAPPER_END,
+                  },
+                  submitText: "Ask",
+                }}
+              />
+            </ActionPanel>
+          }
+        />
+      )}
     </List>
   );
 }
